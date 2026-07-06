475f289152da2916d1f9465039afa6cf1b388371
Catherine-Gunita <Catherine_gunita@dlsu.edu.ph>
2026-07-07 03:34:39 +0800

    minor chnages

--- x86-history/components/view_mainPage/Panel.tsx
+++ x86-history/components/view_mainPage/Panel.tsx
@@ -13,12 +13,12 @@ export default function Panel({image,headText,bodyText,link}:{image?:string,head
                 </div>
                 <h1 className="text-[1.5rem] md:w-[10vw] text-center font-[Georgia] text-white mt-4">{headText}</h1>
             </div>
-            <div className="w-[60vw] h-auto gap-5 p-2 flex flex-col items-center justify-end bg-[#4B5694] rounded-lg shadow-md">
-                <h2 className="text-[clamp(1.50rem,0.5vw+0.8rem,1.75rem)] font-[Perpetua] text-white mt-4">{bodyText}</h2>
+            <div className="w-[60vw] h-auto gap-5 p-2 flex flex-col items-center justify-end rounded-lg shadow-md">
+                <h2 className="text-[clamp(1.50rem,0.5vw+0.8rem,1.75rem)] font-[Perpetua] text-justify text-white mt-4">{bodyText}</h2>
                 
                 <div className="w-full flex md:justify-end items-center justify-center">
                 <Link href={link||"/"}>
-                    <button className="py-2 px-10 text-[clamp(1.50rem,0.5vw+0.8rem,1.75rem)] text-white font-[Perpetua] bg-[#4B5694] hover:bg-[#7288AE] rounded">
+                    <button className="py-2 px-10 text-[clamp(1.50rem,0.5vw+0.8rem,1.75rem)] text-white font-[Perpetua] hover:bg-[#7288AE] rounded">
                         learn more
                     </button>  
                 </Link>     
