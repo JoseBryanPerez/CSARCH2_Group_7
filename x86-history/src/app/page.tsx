f243f1f8fa597a086a5f513aad071437f1600590
Catherine-Gunita <Catherine_gunita@dlsu.edu.ph>
2026-07-07 14:56:26 +0800

    .

--- x86-history/src/app/page.tsx
+++ x86-history/src/app/page.tsx
@@ -34,7 +34,7 @@ export default function page() {
 
     {/*insert the text of the short overview in the insert text here */}
    
-       <h2 className="text-[clamp(1.75rem,2vw+1rem,2.25rem)] text-left font-[Georgia] text-black">Short Overview</h2>
+       <h2 className="text-[clamp(1.75rem,2vw+1rem,2.25rem)] text-left font-[Georgia] text-black">Overview</h2>
        <h2 className="text-[clamp(1.50rem,0.5vw+0.8rem,1.75rem)] text-justify font-[Perpetua] text-black">Assembly Language is a low-level language that allows programmers to communicate directly with computer hardware, offering more speed, space, and capability than most high-level languages. But before x86-64, ARM, MIPS and the more popular assembly languages used today, computer scientists had to communicate directly with hardware using long strings of 0s and 1s. This website aims to give a brief overview on the evolution of the x86-64 instruction set architecture (ISA). </h2>
     
     </div>
