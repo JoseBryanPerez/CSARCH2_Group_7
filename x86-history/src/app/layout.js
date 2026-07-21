60638f1487d6e6ef09627c527515af23d76116a4
Catherine-Gunita <Catherine_gunita@dlsu.edu.ph>
2026-07-21 09:57:13 +0800

    minor changes

--- x86-history/src/app/layout.js
+++ x86-history/src/app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <page>{children}</page>
+        <main>{children}</main>
       </body>
     </html>
   );
